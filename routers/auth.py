1992a3c72d34f42f1a0ebebc306f03160f0097ec
ujjawal <ujjawal17032002@gmail.com>
2025-04-18 11:27:38 +0530

    create login method

--- routers/auth.py
+++ routers/auth.py
@@ -1,14 +1,35 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+from dotenv import load_dotenv
+import jwt
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 import models
 from database import get_db
 from sqlalchemy.orm import Session
-from passlib.hash import pbkdf2_sha256
+from passlib.context import CryptContext
 from uuid import UUID
 
 router = APIRouter(
     prefix="/auth", tags=["auth"], responses={401: {"message": "unauthorized access"}}
 )
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = os.getenv("ALGORITHM")
+
+# PYDANTIC MODE
+
+
+class UserLogin(BaseModel):
+    username: str
+    email: str
+    password: str
 
 
 class UserRegister(BaseModel):
@@ -23,29 +44,21 @@ class UserResponse(BaseModel):
     email: str
     username: str
 
-
-def get_hashed_password(password: str) -> str:
-    return pbkdf2_sha256.hash(password)
-
-
-def verify_hashed_password(password: str, hashed_password: str) -> bool:
-    return pbkdf2_sha256.verify(password, hashed_password)
+    class config:
+        orm_mode = True
 
 
-@router.get("/")
-async def auth():
-    return {"mess": "inside the auth"}
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-# TODO: define different message if pydantic validation failed
-@router.post(
-    "/register", response_model=UserResponse
-)  # response_model is used to define the structure of the data returend from the endpoinit with automatic filternation
-async def register_user(
-    user: UserRegister, db: Session = Depends(get_db)
-):  # UserRegister will be used to verify the json response and create a user object after that
+# HELPERS FUNCTION
 
-    # NOTE:  verify the password first before doing the other validation which require the database query
+
+def validate_user(user, db):
+
+    # NOTE: first check the password then move forward
 
     if user.password != user.confirm_password:
         raise HTTPException(status_code=400, detail="password does not match")
@@ -66,11 +79,62 @@ async def register_user(
     if validation2 is not None:
         raise HTTPException(status_code=400, detail="email already exist")
 
+
+def authenticate_user(username, password, db):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(username: str, user_id: str, expires_delta=Optional[timedelta]):
+
+    # create the payload for the token with "sub", "expire_time"
+
+    # encode it with the jwt method
+
+    to_encode = {"sub": username}
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@router.get("/")
+async def auth():
+    return {"mess": "inside the auth"}
+
+
+# TODO: define different message if pydantic validation failed
+@router.post(
+    "/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)  # response_model is used to define the structure of the data returend from the endpoinit with automatic filternation
+async def register_user(
+    user: UserRegister, db: Session = Depends(get_db)
+):  # UserRegister will be used to verify the json response and create a user object after that
+
+    validate_user(user, db)
+
     try:
         user_model = models.User(
             username=user.username,
             email=user.email,
-            hashed_password=get_hashed_password(user.password),
+            hashed_password=get_password_hash(user.password),
         )
 
         db.add(user_model)
@@ -83,4 +147,29 @@ async def register_user(
         raise HTTPException(
             status_code=500,
             detail=f"Some internal issue occur in registration {str(e)}",
+        )
+
+
+@router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
+async def loginUser(user: UserLogin, db: Session = Depends(get_db)):
+
+    try:
+
+        user = authenticate_user(user.username, user.password, db)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="incorrect username or password",
+            )
+        # now both username and password is correct, create the jwt token
+        access_token_expires = timedelta(minutes=60)
+        access_token = create_access_token(
+            username=user.username, user_id=user.id, expires_delta=access_token_expires
+        )
+
+        return Token(access_token=access_token, token_type="Bearer")
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"some error in token generation, {str(e)}"
         )
